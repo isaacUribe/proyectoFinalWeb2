28dcc3c9a6baa2c2c73890a341735a6673f3fd17
isaacUribe <iuribear@cesde.net>
2023-06-24 14:38:37 -0500

    se conecta con firebase

---
+++ src/components/config/dataBase.jsx
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZveAQR4M0_UjDWsAG--oeHZirFp6KK48",
+  authDomain: "empresa-ce39a.firebaseapp.com",
+  projectId: "empresa-ce39a",
+  storageBucket: "empresa-ce39a.appspot.com",
+  messagingSenderId: "1013112793955",
+  appId: "1:1013112793955:web:a6ec6b47ae3e789b6e4156"
+};
+
+const app = initializeApp(firebaseConfig);
