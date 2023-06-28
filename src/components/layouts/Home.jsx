203733c79a08876d149f08da8249851c002f1e81
jhongarcia09 <menesesgarciajhonwilmar7@gmail.com>
2023-06-28 16:35:48 -0500

    se agregan cambios al home

--- src/components/layouts/Home.jsx
+++ src/components/layouts/Home.jsx
@@ -65,6 +65,17 @@ const Home = () => {
           </section>
           <img src={proveedores} alt="img"/>
         </section>
+        <section className='cuerpo-home1'>
+        <img src={peliculas} alt="img"/>
+          <section className='texto-home1'>
+            <h2>Peliculas</h2>
+            <p>
+            Tambien tenemos un para ofrecerles peliculas de la mejor calidad para ustede y su familia
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quidem quisquam ratione atque deleniti cumque nam velit. Quibusdam doloremque officia repellat? Nulla eaque, inventore ut repellat veniam cum maxime consequuntur.</p>
+          </section>
+          
+        </section>
         
       {/* <section className='cuerpo-home'>
         <div className='seccion-principal'>
