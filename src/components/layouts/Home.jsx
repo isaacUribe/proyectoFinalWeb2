b4406ab6221e761715ee85726ac8031c26b1bc14
jhongarcia09 <menesesgarciajhonwilmar7@gmail.com>
2023-06-28 16:05:33 -0500

    se agregan cambios al home

--- src/components/layouts/Home.jsx
+++ src/components/layouts/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Menu from './Menu'
 import Bienvenidos from "./../img/Bienvenidos.jpg"
+import trabajo from "./../img/trabajo.jpg"
 import empleados from "./../img/empleados.jpg"
 import clientes from "./../img/clientes.png"
 import productos from "./../img/productos.jpg"
@@ -12,25 +13,50 @@ const Home = () => {
     
     <div>
       <Menu/>
-      <hr />
-     <center><img  src={Bienvenidos} alt="img"/></center> 
-     <center><h2>Estamos encantados de tenerte entre nosotros.¡En nombre de todos los miembros y de la dirección, nos gustaría extender nuestra más cálida bienvenida y buenos deseos!</h2></center>
+      <h1 className='titulo'>Empresa Juan Camilo Jaramillo</h1>
+      <section className='cuerpo-home1'>
+          <section className='texto-home1'>
+            <p>
+            Estamos encantados de tenerte entre nosotros.¡En nombre de todos los miembros y de la dirección, nos gustaría extender nuestra más cálida bienvenida y buenos deseos!
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quidem quisquam ratione atque deleniti cumque nam velit. Quibusdam doloremque officia repellat? Nulla eaque, inventore ut repellat veniam cum maxime consequuntur.</p>
+            <img src={Bienvenidos} alt="img"/>
+          </section>
+          <img src={trabajo} alt="img"/>
+        </section>
+        <section className='cuerpo-home1'>
+          <section className='texto-home1'>
+            <h2>Empleados</h2>
+            <p>
+            La energia y la actitud de nuestros empleados siempre es importante
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quidem quisquam ratione atque deleniti cumque nam velit. Quibusdam doloremque officia repellat? Nulla eaque, inventore ut repellat veniam cum maxime consequuntur.</p>
+          </section>
+          <img src={empleados} alt="img"/>
+        </section>
+        
+      {/* <section className='cuerpo-home'>
+        <div className='seccion-principal'>
+        <h4>Estamos encantados de tenerte entre nosotros.¡En nombre de todos los miembros y de la dirección, nos gustaría extender nuestra más cálida bienvenida y buenos deseos!</h4>
+          <img  src={Bienvenidos} alt="img"/>
+        </div> 
+          
+          <pre> <h3>La energia y la actitud de nuestros empleados siempre es importante</h3></pre>
+          <img className="imgemple" src={empleados} alt="img"/>
+
+          <pre><h3>Queremos que nuestros clientes siempre esten satisfechos con nuetro trabajo</h3></pre>
+          <img className="imgemple" src={clientes} alt="img"/>
+
+          <pre><h3>Nuestros productos siempre son de la mejor calidad para nuestros compradores</h3></pre>
+          <img className="imgemple" src={productos} alt="img"/>
+
+          <pre><h3>Nuestros proveedores estan certificados y son unos de los mejores de colombia</h3></pre>
+          <img className="imgemple" src={proveedores} alt="img"/>
+
+          <pre><h3>Tambien tenemos un para ofrecerles peliculas de la mejor calidad para ustede y su familia</h3></pre>
+          <img className="imgemple" src={peliculas} alt="img"/>
+      </section> */}
       
-      <hr />
-      <pre> <h3>La energia y la actitud de nuestros empleados siempre es importante</h3></pre>
-      <img className="imgemple" src={empleados} alt="img"/>
-
-      <pre><h3>Queremos que nuestros clientes siempre esten satisfechos con nuetro trabajo</h3></pre>
-      <img className="imgemple" src={clientes} alt="img"/>
-
-      <pre><h3>Nuestros productos siempre son de la mejor calidad para nuestros compradores</h3></pre>
-      <img className="imgemple" src={productos} alt="img"/>
-
-      <pre><h3>Nuestros proveedores estan certificados y son unos de los mejores de colombia</h3></pre>
-      <img className="imgemple" src={proveedores} alt="img"/>
-
-      <pre><h3>Tambien tenemos un para ofrecerles peliculas de la mejor calidad para ustede y su familia</h3></pre>
-      <img className="imgemple" src={peliculas} alt="img"/>
     </div>
   )
 }
